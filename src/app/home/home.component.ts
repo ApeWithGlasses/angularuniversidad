fbe528d5aa0756c79093c67c52c732a6726dd99a
ProfeCesde <alveragomez@cesde.net>
2023-06-14 10:18:07 -0500

    ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Estudiante } from '../interfaces/Estudiante';
+import { EstudianteService } from '../estudiante.service';
 
 @Component({
   selector: 'app-home',
@@ -10,50 +11,14 @@ export class HomeComponent {
 
   //atributos
   public estudiantes:Estudiante[]=[]
- 
-  //quemando 5 estudaintes
-  public estudiante1:Estudiante={
-    id:1,
-    nombre:"Juan José",
-    correo:"juan@gmail.com",
-    documento:"123456",
-    acudiente:{
-      id:1,
-      nombres:"Jose Gallego",
-      telefono:"32565"
-    }
-  }
 
-  public estudiante2:Estudiante={
-    id:2,
-    nombre:"Sara Sofia",
-    correo:"sara@gmail.com",
-    documento:"123456",
-    acudiente:{
-      id:2,
-      nombres:"Andres Mazo",
-      telefono:"32565"
-    }
-  }
+  public constructor(public peticion:EstudianteService){
 
-  public estudiante3:Estudiante={
-    id:2,
-    nombre:"Carlos Mario",
-    correo:"carlos@gmail.com",
-    documento:"123456",
-    acudiente:{
-      id:3,
-      nombres:"Martha Gallo",
-      telefono:"32565"
-    }
-  }
-
-  public constructor(){
-    //llamamos al servicio y cargamos los estudiantes
-    this.estudiantes.push(this.estudiante1)
-    this.estudiantes.push(this.estudiante2)
-    this.estudiantes.push(this.estudiante3)
-    console.log(this.estudiantes)
+    this.peticion.buscarEstudiantes()
+    .subscribe((respuesta)=>{
+      console.log(respuesta)
+      this.estudiantes=respuesta
+    })
     
   }
 
